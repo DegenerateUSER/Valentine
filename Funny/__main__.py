13bdaa1dc9811a2275ebc5a009cf9f9666968bb9
DegenerateUSER <89341570+DegenerateUSER@users.noreply.github.com>
2022-03-05 19:23:52 +0530

    Update __main__.py

--- Funny/__main__.py
+++ Funny/__main__.py
@@ -208,7 +208,7 @@ def start(update: Update, context: CallbackContext):
                      ]]))
     else:
         update.effective_message.reply_text(
-            "I was always online!"\n"<b>Up since:</b> <code>{}</code>".format(uptime),
+            "I was always online! \n <b>Up since:</b> <code>{}</code>".format(uptime),
             parse_mode=ParseMode.HTML)
 
 
