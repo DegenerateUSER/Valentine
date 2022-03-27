e5244baca344a601092f792b881dc642242facfd
DegenerateUSER <89341570+DegenerateUSER@users.noreply.github.com>
2022-03-27 16:11:12 +0530

    Update alive.py

--- Funny/modules/alive.py
+++ Funny/modules/alive.py
@@ -7,18 +7,18 @@ from pyrogram import __version__ as pyrover
 from Funny.events import register as MEMEK
 from Funny import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/84dbe9bddd4bc9aba78ee.jpg"
+PHOTO = "https://telegra.ph/file/90b4bdd8e5e5bcd0cee14.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  YUMEKO = "**Holla I'm Reze!** \n\n"
+  YUMEKO = "**Hello I'm Phosphophyllite!** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
   YUMEKO += "×**I Have No Owner Just Few Gay Devs Find Them At [GodsValley](https://t.me/GodsValley)**× \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/RezeProBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/KlRAYoshikage")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/PhosphophylliteRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/KlRAYoshikage")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
